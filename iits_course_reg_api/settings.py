08a6e7c4b7460e1fffefa6ebe8ba6cbe7ed21e1b
shz-cod <web.shahidul.alam@gmail.com>
2023-05-03 17:19:36 +0600

    v1.0.1

--- iits_course_reg_api/settings.py
+++ iits_course_reg_api/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY", default="unsafe-secret-key")
-DEBUG = True    
+DEBUG = False    
 
 ALLOWED_HOSTS =  ["*"]
 
